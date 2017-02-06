28cc124c7b81f341980988ab4aea8dc1a70ad62f
Aleksandr Krulov <a.krulov@gmail.com>
2017-02-06 16:09:13 +0300

    create view for employees

--- client/app/bundles/HelloWorld/components/employees/Employees.jsx
+++ client/app/bundles/HelloWorld/components/employees/Employees.jsx
@@ -5,13 +5,41 @@ class Employees extends Component {
     super(props)
   }
 
+  componentWillMount() {
+    this.props.fetchEmployees()
+  }
+
   componentDidMount() {
     this.props.initialUserSubscriptions()
-    this.props.fetchEmployees()
+    // this.props.fetchEmployees()
   }
 
   componentWillUnmount() {
     this.props.cancelAllUserSubscriptions()
+  }
+
+  renderEmployee = (employee) =>
+    <div key={employee.id}>
+      <div className="col-lg-4">
+        <div className="contact-box">
+          <div className="col-sm-4">
+            <img alt="image" className="img-circle m-t-xs img-responsive" src=""></img>
+          </div>
+          <div className="col-sm-8">
+            <h3><strong>{employee.fullname}</strong></h3>
+          </div>
+          <div className="clearfix"></div>
+        </div>
+      </div>
+    </div>
+
+  renderEmployees = () => {
+    if(this.props.employees == '') {
+      return <div className="row">Нет сотрудников</div>
+    } else {
+      return <div className="row">{this.props.employees.map(this.renderEmployee)}</div>
+      // this.props.employees.map(this.renderEmployee)
+    }
   }
 
   render = () => {
@@ -19,7 +47,7 @@ class Employees extends Component {
       <div className="wrapper wrapper-content animated fadeInRight">
         <div className="ibox">
           <div className="ibox-content">
-            Test Employees
+            {this.renderEmployees()}
           </div>
         </div>
       </div>

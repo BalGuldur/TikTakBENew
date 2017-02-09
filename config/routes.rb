678f55d0dfcd594f28565dddc8e2727866a092db
Aleksandr Krulov <a.krulov@gmail.com>
2017-02-09 15:51:22 +0300

    add routes for m_c

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,9 @@ Rails.application.routes.draw do
 
   # Внутренние ссылки API
   api_version(module: "V1", path: {value: "v1"}, default: true) do # RuboCop::Disable Metrics/BlockLength
+    resources :menu_categories, only: [:create, :destroy, :update] do
+      get 'index', on: :collection
+    end
     resources :menu_departments, only: [:create, :destroy, :update] do
       get 'index', on: :collection
     end
